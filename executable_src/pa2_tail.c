b57be29df24eb5ff2807760c89caba13788c43e4
rachelie <rachelie.dwg@gmail.com>
2024-05-05 17:30:44 +0000

    fixed tail

--- executable_src/pa2_tail.c
+++ executable_src/pa2_tail.c
@@ -1,38 +1,106 @@
-#include <ctype.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <getopt.h>
+#include <libgen.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
 #define BUFFER_SIZE 1024
 
 // function prototypes
-int pa2_tail(int argc, char *argv[]);
-void print_tail_help(const char *command);
-void print_tail_error(const char *filename);
+void print_tail(char *file_name, int num_lines) {
+    FILE *fp;
+    char *line = NULL;
+    size_t len = 0;
+    int lines_read = 0;
+    int lines_to_skip = 0;
+    int lines_printed = 0;
+
+    if (strcmp(file_name, "-") == 0) {
+        fp = stdin;
+    } else {
+        fp = fopen(file_name, "r");
+
+        // check for errors
+        if (fp == NULL) {
+            fprintf(stderr, "pa2_tail: cannot open '%s' for reading: %s\n", file_name, strerror(errno));
+            return;
+        }
+
+        // check if file is a directory
+        struct stat st;
+        if (stat(file_name, &st) == 0 && S_ISDIR(st.st_mode)) {
+            fclose(fp);
+            fprintf(stderr, "pa2_tail: cannot open '%s' for reading: Is a directory\n", file_name);
+            return;
+        }
+    }
+
+    // count the number of lines in the file
+    while (getline(&line, &len, fp) != -1) {
+        lines_read++;
+    }
+
+    // calculate the number of lines to skip
+    if (lines_read > num_lines) {
+        lines_to_skip = lines_read - num_lines;
+    }
+
+    // reset file pointer to the beginning
+    fseek(fp, 0, SEEK_SET);
+
+    // move file pointer to the line to start printing from
+    while (lines_to_skip > 0 && getline(&line, &len, fp) != -1) {
+        lines_to_skip--;
+    }
+
+    // print the lines
+    while (getline(&line, &len, fp) != -1) {
+        printf("%s", line);
+        lines_printed++;
+    }
+
+    // free memory and close file
+    if (line != NULL) {
+        free(line);
+    }
+    if (fp != stdin) {
+        fclose(fp);
+    }
+}
 
 // function implementations
-int pa2_tail(int argc, char *argv[]) {
+int main(int argc, char *argv[]) {
     int opt;
     int num_lines = 10;
-    int fd;
 
     while ((opt = getopt(argc, argv, "n:h")) != -1) {
         switch (opt) {
         case 'n':
             // check if num_lines is a valid number
-            if (isdigit(*optarg)) {
-                num_lines = atoi(optarg);
-            } else {
-                fprintf(stderr, "pa2_tail: invalid number of lines: %s\n", optarg);
+            char *end;
+            num_lines = strtol(optarg, &end, 10);
+
+            if (*end != '\0' || num_lines < 0) {
+                fprintf(stderr, "pa2_tail: invalid number of lines: '%s'\n", optarg);
                 return -1;
             }
+
             break;
         case 'h':
-            print_tail_help(argv[0]);
+            printf("pa2_tail Usage: %s [-n number] [file]\n", argv[0]);
+            printf("Output the last part of files\n\n");
+            printf("With more than one FILE, precede each with a header giving the file name.\n\n");
+            printf("Mandatory arguments to long options are mandatory for short options too.\n");
+            printf("  -n, --lines=[-]NUM      output the last NUM lines instead of the last 10;\n");
+            printf("                           with the leading '-', print all but the first\n");
+            printf("                           NUM lines of each file\n");
+            printf("  -h, --help              display this help and exit\n");
             return 0;
         default:
             fprintf(stderr, "Try 'tail --help' for more information.\n");
@@ -40,115 +108,12 @@ int pa2_tail(int argc, char *argv[]) {
         }
     }
 
-    char buffer[BUFFER_SIZE];
-    int lineCount = 0;
-    int bytes_read = 0;
-
-    // setting the file descriptor
     // if no file is given or file is -, read stdin
     if (optind >= argc || strcmp(argv[optind], "-") == 0) {
-        fd = 0;
+        print_tail("-", num_lines);
     } else { // file is given
-        fd = open(argv[optind], O_RDONLY);
-
-        if (fd == -1) {
-            print_tail_error(argv[optind]);
-            return -1;
-        }
+        print_tail(argv[optind], num_lines);
     }
-
-    // seek the end of the file
-    off_t file_size = lseek(fd, 0, SEEK_END);
-    if(file_size == -1) {
-        fprintf(stderr, "pa2_tail: Unable to determine file size\n");
-        if(fd != 0) {
-            close(fd);
-        }
-        return -1;
-    }
-
-    // read the file from the end
-    off_t offset = 0; // offset from the end of the file
-    while(offset < file_size && lineCount < num_lines){
-        // read the file in chunks of BUFFER_SIZE
-        // calculate the read offset which is the offset from the end of the file
-        off_t read_off = file_size - offset - BUFFER_SIZE;
-        if(read_off < 0){
-            read_off = 0;
-        }
-
-        // calculate the number of bytes to read
-        ssize_t bytes_to_read = file_size - offset - read_off;
-        if(bytes_read > BUFFER_SIZE){
-            bytes_read = BUFFER_SIZE;
-        }
-
-        // seek to the read offset
-        if(lseek(fd, read_off, SEEK_SET) == -1){
-            fprintf(stderr, "pa2_tail: Unable to seek to offset %ld\n", read_off);
-            if(fd != 0){
-                close(fd);
-            }
-            return -1;
-        }   
-
-        // read the file
-        ssize_t bytes_being_read = read(fd, buffer, bytes_to_read);
-        if(bytes_being_read == -1){
-            fprintf(stderr, "pa2_tail: Unable to read file\n");
-            if(fd != 0){
-                close(fd);
-            }
-            return -1;
-        }   
-
-        // write the file
-        for(int i = bytes_being_read - 1; i >= 0; i--){
-            write(1, buffer + i, 1);
-            if(buffer[i] == '\n'){
-                lineCount++;
-                if(lineCount == num_lines) break;
-            }
-        }
-
-        offset += bytes_being_read; // update the offset
-    }
-
-    if(fd != 0) close(fd); // close the file descriptor
 
     return 0;
-}
-
-
-void print_tail_help(const char *command) {
-    printf("pa2_tail Usage: %s [-n number] [file]\n", command);
-    printf("Output the last part of files\n\n");
-    printf("With more than one FILE, precede each with a header giving the file name.\n\n");
-    printf("Mandatory arguments to long options are mandatory for short options too.\n");
-    printf("  -n, --lines=[-]NUM      output the last NUM lines instead of the last 10;\n");
-    printf("                           with the leading '-', print all but the first\n");
-    printf("                           NUM lines of each file\n");
-    printf("  -h, --help              display this help and exit\n");
-}
-
-void print_tail_error(const char *filename) {
-    switch (errno) {
-    case EACCES: // permission denied
-        fprintf(stderr, "pa2_tail: cannot open '%s' for reading: Permission denied\n", filename);
-        break;
-    case EISDIR: // file is a directory
-        fprintf(stderr, "pa2_tail: cannot open '%s' for reading: Is a directory\n", filename);
-        break;
-    case ENOENT: // file does not exist
-        fprintf(stderr, "pa2_tail: cannot open '%s' for reading: No such file or directory\n", filename);
-        break;
-    case ENOTDIR: // not a directory
-        fprintf(stderr, "pa2_tail: cannot open '%s' for reading: Not a directory\n", filename);
-        break;
-    case EPERM: // operation not permitted
-        fprintf(stderr, "pa2_tail: cannot open '%s' for reading: Operation not permitted\n", filename);
-        break;
-    default: //[ ]: check if this is the correct err msg for other errors
-        fprintf(stderr, "pa2_tail: %s\n", strerror(errno));
-    }
 }
