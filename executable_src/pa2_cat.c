9598b0b87920e36ba3b4b41eb8f73eff4560e40c
rachelie <rachelie.dwg@gmail.com>
2024-05-05 15:56:44 +0000

    works

--- executable_src/pa2_cat.c
+++ executable_src/pa2_cat.c
@@ -1,71 +1,72 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#include <fcntl.h>
 #include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
 #include <errno.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <libgen.h>
+#include <getopt.h>
+
+void print_file(const char *filename){
+    FILE *fp;
+
+    if(strcmp(filename, "-") == 0){
+        fp = stdin;
+    }else{
+        fp = fopen(filename, "r");
+
+        if(fp == NULL){
+            fprintf(stderr, "pa2_cat: %s: No such file or directory\n", filename);
+            return;
+        }
+
+        struct stat st;
+        if(stat(filename, &st) == 0 && S_ISDIR(st.st_mode)){
+            fclose(fp);
+            fprintf(stderr, "pa2_cat: %s: Is a directory\n", filename);
+            return;
+        }
+    }
+
+    int c;
+    while((c = fgetc(fp)) != EOF){
+        putchar(c);
+        fflush(stdout);
+    }
+
+    if(fp != stdin){
+        fclose(fp);
+    }
+}
 
 int main(int argc, char *argv[]) {
     int opt;
-    while((opt = getopt(argc, argv, "h")) != -1){
-        switch(opt){
-            case 'h':
-                printf("pa2_cat Usage: %s [file1] [file2] ...\n", argv[0]);
-                printf("Concatenate FILE(s) to standard output.\n\n");
-                printf("With no FILE, or when FILE is -, read standard input.\n\n");
-                printf("Mandatory arguments to long options are mandatory for short options too.\n");
-                printf("  -h, --help     display this help and exit\n");
-                return 0;
-            default:
-                fprintf(stderr, "Try 'cat --help' for more information.\n");
-                return -1;
+    while ((opt = getopt(argc, argv, "h")) != -1) {
+        switch (opt) {
+        case 'h':
+            printf("pa2_cat Usage: %s [file1] [file2] ...\n", argv[0]);
+            printf("Concatenate FILE(s) to standard output.\n\n");
+            printf("With no FILE, or when FILE is -, read standard input.\n\n");
+            printf("Mandatory arguments to long options are mandatory for short options too.\n");
+            printf("  -h, --help     display this help and exit\n");
+            return 0;
+        default:
+            fprintf(stderr, "pa2_cat: invalid option -- '%c'\n", opt);
+            fprintf(stderr, "Try 'cat --help' for more information.\n");
+            return -1;
         }
     }
-    
-    
-    char buffer[1024];
-    int fd;
 
-    if(argc == 1 || !strcmp(argv[0], "-")) { // read from stdin 
-        // [ ] check if this is an infinite loop
-        while(read(0, buffer, 1) > 0)
-            write(1, buffer, 1);
+    if (argc == 1 || !strcmp(argv[1], "-")) { // read from stdin
+        print_file("-");
     } else {
         // read from file (multiple files possible)
-        for(int i = 1; i < argc; i++){
-            fd = open(argv[i], O_RDONLY);
-            
-            // error handling
-            if(fd == -1){
-                switch(errno){
-                    case EACCES: // permission denied
-                        fprintf(stderr, "pa2_cat: %s: Permission denied\n", argv[i]);
-                        break;
-                    case EISDIR: // file is a directory
-                        fprintf(stderr, "pa2_cat: %s: Is a directory\n", argv[i]);
-                        break;
-                    case ENOENT: // file does not exist
-                        fprintf(stderr, "pa2_cat: %s: No such file or directory\n", argv[i]);
-                        break;
-                    case ENOTDIR: // not a directory
-                        fprintf(stderr, "pa2_cat: %s: Not a directory\n", argv[i]);
-                        break;
-                    case EPERM: // operation not permitted
-                        fprintf(stderr, "pa2_cat: %s: Operation not permitted\n", argv[i]);
-                        break;
-                    default: 
-                        fprintf(stderr, "pa2_cat: %s: %s\n", argv[i], strerror(errno));
-                }
-
-                return -1; // exit with error
-            }
-
-            while(read(fd, buffer, 1) > 0){
-                write(1, buffer, 1);
-            }
-
-            if(fd != 0) close(fd);
-        }
+        for (int i = optind; i < argc; i++)
+            print_file(argv[i]);
     }
-    
+
     return 0;
 }
--- executable_src/pa2_head.c
+++ executable_src/pa2_head.c
@@ -1,37 +1,82 @@
-#include <ctype.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <libgen.h>
+#include <getopt.h>
 
 #define BUFFER_SIZE 1024
 
 // function prototypes
-void print_head_help(const char *command);
-void print_head_error(const char *filename);
+void print_head(char *filename, int num_lines) {
+    FILE *fp;
+    char* line = NULL;
+    int lines_printed = 0;
+    int c;
+
+    if (strcmp(filename, "-") == 0) {
+        fp = stdin;
+    } else {
+        fp = fopen(filename, "r");
+
+        if(fp == NULL){
+            fprintf(stderr, "pa2_head: cannot open '%s' for reading: %s\n", filename, strerror(errno));
+            return;
+        }
+
+        struct stat st;
+        if(stat(filename, &st) == 0 && S_ISDIR(st.st_mode)){
+            fclose(fp);
+            fprintf(stderr, "pa2_head: cannot open '%s' for reading: Is a directory\n", filename);
+            return;
+        }
+    }
+
+    while(lines_printed < num_lines && getline(&line, &c, fp) != -1){
+        printf("%s", line);
+        lines_printed++;
+    }
+
+    if(line != NULL){
+        free(line);
+    }
+    if(fp != stdin){
+        fclose(fp);
+    }
+}
 
 // function implementations
 int main(int argc, char *argv[]) {
     int opt;
     int num_lines = 10;
-    int fd;
 
     while ((opt = getopt(argc, argv, "n:h")) != -1) {
         switch (opt) {
         case 'n': // set the number of lines to print
             // check if num_lines is a valid number
-            if (isdigit(*optarg)) {
-                num_lines = atoi(optarg);
-            } else {
+            char* end;
+            num_lines = strtol(optarg, &end, 10);
+
+            if (*end != '\0' || num_lines < 0) {
                 fprintf(stderr, "pa2_head: invalid number of lines: %s\n", optarg);
                 return -1;
             }
+
             break;
         case 'h': // print the help message
-            print_head_help(argv[0]);
+            printf("pa2_head Usage: %s [-n number] [file]\n", argv[0]);
+            printf("Output the first part of files\n\n");
+            printf("With more than one FILE, precede each with a header giving the file name.\n\n");
+            printf("Mandatory arguments to long options are mandatory for short options too.\n");
+            printf("  -n, --lines=[-]NUM      output the first NUM lines instead of the first 10;\n");
+            printf("                           with the leading '-', print all but the last\n");
+            printf("                           NUM lines of each file\n");
+            printf("  -h, --help              display this help and exit\n");
             return 0;
         default:
             fprintf(stderr, "Try 'head --help' for more information.\n");
@@ -39,79 +84,14 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    char buffer[BUFFER_SIZE];
-    int lineCount = 0;
-    int bytes_read = 0;
     // if no file is given or file is -, read stdin
     if (optind >= argc || strcmp(argv[optind], "-") == 0) {
-        // [ ] check if this runs the correct amount of lines
-        while ((bytes_read = read(0, buffer, 1)) > 0) {
-            for (int i = 0; i < bytes_read; i++) {
-                write(1, buffer, 1);
-
-                if (buffer[i] == '\n') {
-                    lineCount++;
-                    if (lineCount == num_lines) break;
-                }
-            }
-
-            if (lineCount == num_lines) break;
-        }
-    } else { // file is given
-        fd = open(argv[optind], O_RDONLY);
-
-        if (fd == -1) {
-            print_head_error(argv[optind]);
-            return -1;
-        }
-
-        while ((bytes_read = read(fd, buffer, 1)) > 0) {
-            for (int i = 0; i < bytes_read; i++) {
-                write(1, buffer, 1);
-                if (buffer[i] == '\n') {
-                    lineCount++;
-                    if (lineCount == num_lines) break;
-                }
-            }
-
-            if (lineCount == num_lines) break;
-        }
-
-        if (fd != 0) close(fd);
+        print_head("-", num_lines);
+    } 
+    // file is given
+    else { 
+        print_head(argv[optind], num_lines);
     }
 
     return 0;
-}
-
-void print_head_help(const char *command) {
-    printf("pa2_head Usage: %s [-n number] [file]\n", command);
-    printf("Output the first part of files\n\n");
-    printf("With more than one FILE, precede each with a header giving the file name.\n\n");
-    printf("Mandatory arguments to long options are mandatory for short options too.\n");
-    printf("  -n, --lines=[-]NUM      output the first NUM lines instead of the first 10;\n");
-    printf("                           with the leading '-', print all but the last\n");
-    printf("                           NUM lines of each file\n");
-    printf("  -h, --help              display this help and exit\n");
-}
-
-void print_head_error(const char *filename) {
-    switch (errno) {
-    case EACCES: // permission denied
-        fprintf(stderr, "pa2_head: cannot open '%s' for reading: Permission denied\n", filename);
-        break;
-    case EISDIR: // file is a directory
-        fprintf(stderr, "pa2_head: cannot open '%s' for reading: Is a directory\n", filename);
-        break;
-    case ENOENT: // file does not exist
-        fprintf(stderr, "pa2_head: cannot open '%s' for reading: No such file or directory\n", filename);
-        break;
-    case ENOTDIR: // not a directory
-        fprintf(stderr, "pa2_head: cannot open '%s' for reading: Not a directory\n", filename);
-        break;
-    case EPERM: // operation not permitted
-        fprintf(stderr, "pa2_head: cannot open '%s' for reading: Operation not permitted\n", filename);
-        break;
-    default: // [ ]: check if this is the correct err msg for other errors
-        fprintf(stderr, "pa2_head: %s\n", strerror(errno));
-    }
 }
