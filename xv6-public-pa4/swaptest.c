45321214ad21d2dfd610fde6b8ecac11e24dbebf
rachelie <rachelie.dwg@gmail.com>
2024-05-29 02:04:11 +0900

    pa4: introduced fork (sbrk-) doesn't work

--- xv6-public-pa4/swaptest.c
+++ xv6-public-pa4/swaptest.c
@@ -16,8 +16,37 @@ int main () {
     printf(1, "Swap space: %d/%d\n", a, b);
 
     // sbrk loop
-    for (int i = 0; i < 60000; i++) {
+    for (int i = 0; i < 30000; i++) {
         void *p = sbrk(4096);
+        if (p == (void *)-1) {
+            printf(1, "sbrk failed\n");
+            break;
+        }
+    }
+
+    swapstat(&a, &b);
+    printf(1, "Swap space: %d/%d\n", a, b);
+
+    int pid = fork();
+    if(pid == 0) {
+        for (int i = 0; i < 30000; i++) {
+            void *p = sbrk(4096);
+            if (p == (void *)-1) {
+                printf(1, "sbrk failed\n");
+                break;
+            }
+        }
+
+        printf(1, "Child Swap space: %d/%d\n", a, b);
+
+        exit();
+    }else{
+        wait();
+    }
+
+    // un-sbrk loop
+    for (int i = 0; i < 60000; i++) {
+        void *p = sbrk(-4096);
         if (p == (void *)-1) {
             printf(1, "sbrk failed\n");
             break;
