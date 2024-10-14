79b9ebddff2236a04bb6a1eb7b31a787a7349c62
racheliee <rachelie.dwg@gmail.com>
2024-10-14 00:31:43 +0000

    fix: change part1 to use double from float

--- part1/part1.py
+++ part1/part1.py
@@ -25,12 +25,7 @@ def homework_loop_sequential_source(chain_length, unroll_factor):
     function_body = []
     function_body.append(f" for (int i = 0; i < size; i+={unroll_factor}) {{")
     for i in range(unroll_factor):
-        function_body.append(f'     float tmp{i} = b[i + {i}];')
-        # function_body.append(f'     float tmp{i} = b[i];')
-        # function_body.append(f'     for(int j = 1; j <= {chain_length}; ++j) {{')
-        # function_body.append(f'         tmp{i} += (float)j;')
-        # function_body.append(f'     }}')
-        # function_body.append(f'     b[i] = tmp{i};')
+        function_body.append(f'     double tmp{i} = b[i + {i}];')
         for j in range(1, chain_length+1):
             function_body.append(f'     tmp{i} += {j}.0f;')
         function_body.append(f'     b[i + {i}] = tmp{i};')
@@ -56,7 +51,7 @@ def homework_loop_interleaved_source(chain_length, unroll_factor):
     
     # write out the `float tmp{i} = b[i];` for each sequence
     for i in range(unroll_factor):
-        function_body.append(f'     float tmp{i} = b[i + {i}];')
+        function_body.append(f'     double tmp{i} = b[i + {i}];')
     
     # write out the sequence of instructions for each sequence
     for i in range(1, chain_length + 1):
