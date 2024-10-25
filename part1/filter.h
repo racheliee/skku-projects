e2980d67988a2b07752403745b59fde9a3a08f6b
racheliee <rachelie.dwg@gmail.com>
2024-10-25 01:47:28 +0000

    feat: add memory_order_relaxed

--- part1/filter.h
+++ part1/filter.h
@@ -16,20 +16,20 @@ class mutex {
         _victim = new atomic<int>[num_threads];
 
         for (int i = 0; i < num_threads; ++i) {
-            _level[i].store(0);
-            _victim[i].store(0);
+            _level[i].store(0, memory_order_relaxed);
+            _victim[i].store(0, memory_order_relaxed);
         }
     }
 
     void lock(int thread_id) {
         for (int i = 1; i < _num_threads; ++i) {
-            _level[thread_id].store(i);
-            _victim[i].store(thread_id);
+            _level[thread_id].store(i, memory_order_relaxed);
+            _victim[i].store(thread_id, memory_order_relaxed);
 
             for (int j = 0; j < _num_threads; ++j) {
                 if (j == thread_id)
                     continue;
-                while (_level[j].load() >= i && _victim[i].load() == thread_id) {
+                while (_level[j].load(memory_order_relaxed) >= i && _victim[i].load(memory_order_relaxed) == thread_id) {
                     this_thread::yield();
                 };
             }
@@ -37,7 +37,7 @@ class mutex {
     }
 
     void unlock(int thread_id) {
-        _level[thread_id].store(0);
+        _level[thread_id].store(0, memory_order_relaxed);
     }
 
   private:
