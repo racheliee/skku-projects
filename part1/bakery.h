e789aea14fcde44d3a9eae2f2bd0604d73eeb270
racheliee <rachelie.dwg@gmail.com>
2024-10-25 01:40:11 +0000

    feat: change to regular arrays

--- part1/bakery.h
+++ part1/bakery.h
@@ -2,7 +2,6 @@
 
 #include <algorithm>
 #include <atomic>
-#include <memory>
 #include <thread>
 #include <vector>
 
@@ -14,51 +13,51 @@ class mutex {
 
     void init(int num_threads) {
         _num_threads = num_threads;
-        _entering.resize(num_threads);
-        _number.resize(num_threads);
+        _entering = new atomic<int>[num_threads];
+        _number = new atomic<int>[num_threads];
 
         for (int i = 0; i < num_threads; ++i) {
-            _entering[i] = make_unique<atomic<int>>(0);
-            _number[i] = make_unique<atomic<int>>(0);
+            _entering[i].store(0);
+            _number[i].store(0);
         }
     }
 
     void lock(int thread_id) {
-        _entering[thread_id]->store(1);
+        _entering[thread_id].store(1);
 
         int max_ticket = 0;
         for (int i = 0; i < _num_threads; ++i) {
-            int t = _number[i]->load();
+            int t = _number[i].load();
             if (t > max_ticket)
                 max_ticket = t;
         }
 
-        _number[thread_id]->store(1 + max_ticket);
+        _number[thread_id].store(1 + max_ticket);
 
-        _entering[thread_id]->store(0);
+        _entering[thread_id].store(0);
 
         for (int j = 0; j < _num_threads; ++j) {
             if (j == thread_id)
                 continue;
 
-            while (_entering[j]->load()) {
+            while (_entering[j].load()) {
                 this_thread::yield();
             };
 
-            while (_number[j]->load() != 0 &&
-                  (_number[j]->load() < _number[thread_id]->load() ||
-                  (_number[j]->load() == _number[thread_id]->load() && j < thread_id))) {
+            while (_number[j].load() != 0 &&
+                   (_number[j].load() < _number[thread_id].load() ||
+                    (_number[j].load() == _number[thread_id].load() && j < thread_id))) {
                 this_thread::yield();
             };
         }
     }
 
     void unlock(int thread_id) {
-        _number[thread_id]->store(0);
+        _number[thread_id].store(0);
     }
 
   private:
     int _num_threads;
-    vector<unique_ptr<atomic<int>>> _entering;
-    vector<unique_ptr<atomic<int>>> _number;
+    atomic<int> *_entering;
+    atomic<int> *_number;
 };
--- part1/filter.h
+++ part1/filter.h
@@ -1,9 +1,8 @@
 #pragma once
 
 #include <atomic>
-#include <memory>
-#include <vector>
 #include <thread>
+#include <vector>
 
 using namespace std;
 
@@ -13,36 +12,36 @@ class mutex {
 
     void init(int num_threads) {
         _num_threads = num_threads;
-        _level.resize(num_threads);
-        _victim.resize(num_threads);
+        _level = new atomic<int>[num_threads];
+        _victim = new atomic<int>[num_threads];
 
         for (int i = 0; i < num_threads; ++i) {
-            _level[i] = make_unique<atomic<int>>(0);
-            _victim[i] = make_unique<atomic<int>>(0);
+            _level[i].store(0);
+            _victim[i].store(0);
         }
     }
 
     void lock(int thread_id) {
         for (int i = 1; i < _num_threads; ++i) {
-            _level[thread_id]->store(i);
-            _victim[i]->store(thread_id);
+            _level[thread_id].store(i);
+            _victim[i].store(thread_id);
 
             for (int j = 0; j < _num_threads; ++j) {
                 if (j == thread_id)
                     continue;
-                while (_level[j]->load() >= i && _victim[i]->load() == thread_id) {
-                  this_thread::yield();
+                while (_level[j].load() >= i && _victim[i].load() == thread_id) {
+                    this_thread::yield();
                 };
             }
         }
     }
 
     void unlock(int thread_id) {
-        _level[thread_id]->store(0);
+        _level[thread_id].store(0);
     }
 
   private:
     int _num_threads;
-    vector<unique_ptr<atomic<int>>> _level;
-    vector<unique_ptr<atomic<int>>> _victim;
+    atomic<int> *_level;
+    atomic<int> *_victim;
 };
