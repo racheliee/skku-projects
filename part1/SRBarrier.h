d6ff6f75b454cc7de566e5b635d132df9a8d3b44
racheliee <rachelie.dwg@gmail.com>
2024-12-03 23:00:38 +0000

    fix: part1.2 based on slides

--- part1/SRBarrier.h
+++ part1/SRBarrier.h
@@ -1,38 +1,40 @@
 #include <atomic>
+#include <vector>
 
 using namespace std;
 
 class barrier_object {
- public:
-  barrier_object() {
-    // Probably don't need to do anything here.
-  }
-
-  void init(int num_threads) {
-    // Implement me
-    size = num_threads;
-    count.store(num_threads);
-    sense.store(false);
-  }
-
-  void barrier(int tid) {
-    // Implement me
-    static thread_local bool my_sense = !sense.load();
-    int pos = count.fetch_add(-1);
-
-    if (pos == 1) {
-      count.store(size);
-      sense.store(my_sense);
-    }else{
-      while (sense.load() != my_sense);
+  public:
+    barrier_object() {
+        // Probably don't need to do anything here.
     }
 
-    my_sense = !my_sense;
-  }
+    void init(int num_threads) {
+        // Implement me
+        this->num_threads = num_threads;
+        counter.store(0);
+        sense.store(false);
+        thread_sense.resize(num_threads, true);
+    }
 
-private:
-  // Give me some private variables
-  atomic<int> count;
-  int size;
-  atomic<bool> sense;
+    void barrier(int tid) {
+        // Implement me
+        int arrival = counter.fetch_add(1) + 1;
+        if (arrival == num_threads) {
+            counter.store(0);
+            sense = thread_sense[tid];
+        } else {
+            while (sense != thread_sense[tid])
+                ;
+        }
+
+        thread_sense[tid] = !thread_sense[tid];
+    }
+
+  private:
+    // Give me some private variables
+    atomic<int> counter;
+    int num_threads;
+    atomic<bool> sense;
+    vector<bool> thread_sense;
 };
