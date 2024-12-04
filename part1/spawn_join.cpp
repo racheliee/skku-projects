8241aec667cfa9ac1bf9a92347bb37ce579659ed
racheliee <rachelie.dwg@gmail.com>
2024-12-04 00:35:22 +0000

    fix: go back to old version for part 1.1

--- part1/spawn_join.cpp
+++ part1/spawn_join.cpp
@@ -3,12 +3,10 @@
 #include <vector>
 #include <cmath>
 
-using namespace std;
-
 // Function for each thread to perform blur on its chunk of the array
 void blur_chunk(double* input, double* output, int start, int end) {
     for (int i = start; i < end; ++i) {
-        output[i] = (input[i - 1] + input[i] + input[i + 1]) / 3;
+        output[i] = (input[i - 1] + input[i] + input[i + 1]) / 3.0;
     }
 }
 
@@ -17,14 +15,15 @@ void blur_chunk(double* input, double* output, int start, int end) {
 // At the end, "output" should contain the final blurred values.
 void repeated_blur(double* input, double* output, int size, int repeats, int num_threads) {
     // Spawn and join threads repeatedly.
-    int chunk_size = size / num_threads; // Divide the array into chunks excluding boundaries
+    int chunk_size = (size - 2) / num_threads; // Divide the array into chunks excluding boundaries
 
     for (int r = 0; r < repeats; ++r) {
-        vector<thread> threads;
+        std::vector<std::thread> threads;
 
         for (int t = 0; t < num_threads; ++t) {
             int start = 1 + t * chunk_size; // Start index for this thread
-            int end = (t == num_threads - 1) ? (t * 1) * chunk_size -1 : (t * 1) * chunk_size + 1; // End index for this thread
+            int end = (t == num_threads - 1) ? (size - 1) : (start + chunk_size); // End index for this thread
+
             threads.emplace_back(blur_chunk, input, output, start, end);
         }
 
@@ -35,5 +34,12 @@ void repeated_blur(double* input, double* output, int size, int repeats, int num
 
         // Swap input and output arrays
         std::swap(input, output);
+    }
+
+    // If the number of repeats is odd, swap back the results to the output array
+    if (repeats % 2 == 1) {
+        for (int i = 0; i < size; ++i) {
+            output[i] = input[i];
+        }
     }
 }
