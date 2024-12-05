003a2a1de991b2d6a9d7ffc40e6f8797521969f1
racheliee <rachelie.dwg@gmail.com>
2024-12-05 22:10:08 +0000

    fix: part2

--- part2/sc_sb.cpp
+++ part2/sc_sb.cpp
@@ -9,14 +9,14 @@ void t0_function(atomic_int* x, atomic_int* y, int* r0, barrier_object* B) {
   // complete me!
   B->barrier(0);
   x->store(1, memory_order_seq_cst);
-  *r0 = y->load(memory_order_seq_cst);
   B->barrier(0);
+  *r0 = y->load(memory_order_seq_cst);
 }
 
 void t1_function(atomic_int* x, atomic_int* y, int* r1,  barrier_object* B) {
   // complete me!
   B->barrier(1);
   y->store(1, memory_order_seq_cst);
-  *r1 = x->load(memory_order_seq_cst);
   B->barrier(1);
+  *r1 = x->load(memory_order_seq_cst);
 }
